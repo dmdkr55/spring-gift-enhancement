ccdba0e606d3b073dc17832b0ecabd5a025023bc
dmdkr55 <59314089+dmdkr55@users.noreply.github.com>
2025-07-11 13:20:08 +0900

    충남대학교 BE 김재혁 위시리스트 2단계 (#246)
    * feat: 기본 코드 준비
    
    * feat: 유효성 검사
    
    * feat: 예외 처리
    
    - GlobalExceptionHandler
    예외 발생시 응답 메시지 제공
    
    * feat: 테스트 코드 추가
    
    - ProductControllerTest
    유효성 검사 및 예외 처리에 대한 테스트 코드 추가
    
    * feat: Admin 화면에서의 유효성 검사 추가
    
    - ProductAdminController
    유효성 검사 및 예외 처리에 대한 코드 추가
    -create-form.html, edit-form.html
    예외 처리에 따른 Exception 메시지
    
    * refactor: 이미지 URL 유효성 검사
    
    - Product.java
    이미지 URL에 대한 pattern을 지정
    
    * refactor: 상품이름 유효성 검사 수정
    
    - Product.java
    '카카오'가 포함된 이름은 needsMdApproval 필드를 true로 하여, MD의 승인이 필요하도록 수정.
    
    * refactor: 변경된 내용에 따라 테스트 코드 수정
              없는 id의 상품 단건 조회시 오류 수정.
    
    - ProductControllerTest.java
    수정된 유효성 검사에 따라 테스트 코드 수정
    존재하지 않는 상품 조회에 대한 테스트 추가
    - ProductRepository.java
    존재하지 않는 상품 조회에 대한 에러 처리
    
    * feat: 회원가입 기능 구현
    
    * feat: 로그인 기능 구현
    
    * feat: 회원가입 기능 구현
    
    * feat: 로그인 기능 구현
    
    * feat: 관리자 페이지에서 멤버 추가 기능
    
    * refactor: 토큰 생성 메소드 분리
    
    - MemberService.java
    secretKey 중복 제거.
    토큰 생성 중복 코드 메소드로 분리
    
    * refactor: Exception Handler 추가
    
    - GlobalExceptionHandler.java
    회원가입, 로그인 시 발생가능한 예외에 대한 핸들러들을 분리
    
    * feat: Admin API에 JWT 인증 필터 적용
    
    - JwtAuthenticationFilter.java, FilterConfig.java
    Admin API 요청에 대해 JWT 인증 로직 추가

--- src/main/java/gift/filter/JwtAuthenticationFilter.java
+++ src/main/java/gift/filter/JwtAuthenticationFilter.java
@@ -1,7 +1,10 @@
 package gift.filter;
 
+<<<<<<< HEAD
 import gift.util.JwtUtil;
 import io.jsonwebtoken.Claims;
+=======
+>>>>>>> ade5d96 (충남대학교 BE 김재혁 위시리스트 2단계 (#246))
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
@@ -13,11 +16,15 @@ import java.io.IOException;
 
 public class JwtAuthenticationFilter implements Filter {
 
+<<<<<<< HEAD
     private final JwtUtil jwtUtil;
 
     public JwtAuthenticationFilter(JwtUtil jwtUtil) {
         this.jwtUtil = jwtUtil;
     }
+=======
+    private static final String secretKey = "Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=";
+>>>>>>> ade5d96 (충남대학교 BE 김재혁 위시리스트 2단계 (#246))
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
@@ -28,6 +35,7 @@ public class JwtAuthenticationFilter implements Filter {
 
         String uri = request.getRequestURI();
 
+<<<<<<< HEAD
         // wishlist 경로 토큰 검사. 로그인이 되어야 이용 가능한 기능
         if (uri.startsWith("/api/wishes")) {
 
@@ -51,9 +59,37 @@ public class JwtAuthenticationFilter implements Filter {
             chain.doFilter(req, res);
         } else {
             // wishlist 경로가 아니면 그냥 통과
+=======
+        // admin 경로만 토큰 검사
+        if (uri.startsWith("/admin/members")) {
+            String token = request.getHeader("Authorization");
+            if (token == null || !isValidToken(token)) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.getWriter().write("Unauthorized Token");
+                return;
+            }
+
+            chain.doFilter(req, res);
+        } else {
+            // admin 경로가 아니면 그냥 통과
+>>>>>>> ade5d96 (충남대학교 BE 김재혁 위시리스트 2단계 (#246))
             chain.doFilter(req, res);
         }
     }
 
+<<<<<<< HEAD
+=======
+    private boolean isValidToken(String token) {
+        try {
+            Jwts.parser()
+                .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                .build()
+                .parseClaimsJws(token); // 파싱되면 유효한 토큰
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+>>>>>>> ade5d96 (충남대학교 BE 김재혁 위시리스트 2단계 (#246))
 
 }
