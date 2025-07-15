2063f32c743bf5b5357c7b140297c2735f3e13ab
Jaehyeok Kim <bbabbasi0505@naver.com>
2025-07-15 16:57:28 +0900

    refactor: Member 관련 코드를 JPA 기반으로 리팩토링

--- src/main/java/gift/model/Member.java
+++ src/main/java/gift/model/Member.java
@@ -1,19 +1,27 @@
 package gift.model;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "Member")
 public class Member {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "이메일은 필수 입력 값입니다.")
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
 
-    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+    @Column(name = "password", nullable = false)
     private String password;
 
     public Member() {
-
     }
 
     public Member(Long id, String email, String password) {
@@ -23,8 +31,7 @@ public class Member {
     }
 
     public Member(String email, String password) {
-        this.email = email;
-        this.password = password;
+        this(null, email, password);
     }
 
     public Long getId() {
